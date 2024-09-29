d37e182666951683c6930cbbfda77ef45daa3b67
devahsanulhuda <devahsanulhuda@gmail.com>
2024-09-29 20:57:51 +0600

    apple-website

---
+++ src/utils/animations.js
@@ -0,0 +1,32 @@
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  secondTarget,
+  animationProps
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 2,
+    ease: "power@.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
